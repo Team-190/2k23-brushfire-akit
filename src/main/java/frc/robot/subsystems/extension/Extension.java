23a0085f235607371f440830a1bdd815d953df05
jshaughnessy24 <jshaughnessy@wpi.edu>
2023-12-07 23:27:14 -0500

    Finished? extension but haven't tested

--- src/main/java/frc/robot/subsystems/extension/Extension.java
+++ src/main/java/frc/robot/subsystems/extension/Extension.java
@@ -1,14 +1,64 @@
 package frc.robot.subsystems.extension;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
 
 public class Extension extends SubsystemBase {
-  // public static final Rotation2d STARTING_POSITION = ;
-  // public static final Rotation2d IDLE_POSITION = ;
+  private final ExtensionIO io;
+  private final ExtensionIOInputsAutoLogged inputs = new ExtensionIOInputsAutoLogged();
+  private final SimpleMotorFeedforward ffModel;
 
-  // public static final LoggedTunableNumber MAX_VELOCITY =
-  //   new LoggedTunableNumber("Pivot/maxVelocity", 600.0);
+  public Extension(ExtensionIO io) {
+    this.io = io;
 
-  // private final ExtensionIO io;
-  // private final ExtensionIOInputsAutoLogged inputs = new ExtensionIOInputsAutoLogged();
+    switch (Constants.currentMode) {
+      case REAL:
+      case REPLAY:
+        ffModel = new SimpleMotorFeedforward(0.1, 0.05);
+        break;
+      case SIM:
+        ffModel = new SimpleMotorFeedforward(0.0, 0.03);
+        break;
+      default:
+        ffModel = new SimpleMotorFeedforward(0.0, 0.0);
+        break;
+    }
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Extension", inputs);
+  }
+
+  /** Run closed loop at the specified velocity. */
+  public void runVelocity(double velocityRPM) {
+    Logger.recordOutput("Extension/SetpointRPM", velocityRPM);
+  }
+
+  /** Stops the flywheel. */
+  public void stop() {
+    io.stop();
+  }
+
+  /** Returns the current velocity in RPM. */
+  @AutoLogOutput
+  public double getVelocityRPM() {
+    return Units.radiansPerSecondToRotationsPerMinute(inputs.velocityRadPerSec);
+  }
+
+  /** Runs forwards at the commanded voltage. */
+  public void runCharacterizationVolts(double volts) {
+    io.setVoltage(volts);
+  }
+
+  /** Returns the average drive velocity in radians/sec. */
+  public double getCharacterizationVelocity() {
+    return inputs.velocityRadPerSec;
+  }
 }
